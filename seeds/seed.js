3ec0097d9a9c776ff42f023f30bd34570570aef8
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-06-20 23:07:01 -0400

    resolved issue with seeds file

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,6 +1,5 @@
-require("dotenv").config({
-    path: '../.env'
-  })
+require('dotenv').config({ path: '../.env' });
+
   const sequelize = require('../config/connection');
   const { User, Article, Comment } = require('../models');
   

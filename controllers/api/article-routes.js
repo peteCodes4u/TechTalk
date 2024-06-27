7177ced3a84df7a73a717ff1bfee8728d56dc9c3
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-06-27 14:32:38 -0400

    Completed api to pull article records by user_id in the article-routes.js and eliminated etraneous comments handlebars

--- controllers/api/article-routes.js
+++ controllers/api/article-routes.js
@@ -1,13 +1,24 @@
 const router = require('express').Router();
 const { Article } = require('../../models');
 
-router.get('/', async(req, res) => {
-try{
-    const articleData = await Article.findAll(
-        {include: [{all: true, nested: true }]}
-    );
-    res.status(200).json((articleData));
-}catch(err){res.status(500).json(err)}
+// get all articles
+router.get('/', async (req, res) => {
+    try {
+        const articleData = await Article.findAll(
+            { include: [{ all: true, nested: true }] }
+        );
+        res.status(200).json((articleData));
+    } catch (err) { res.status(500).json(err) }
+});
+
+// get articles for dashboard by user_id
+router.get('/:user_id', async (req, res) => {
+    try {
+        const dbArticlesData = await Article.findAll({
+            where: { user_id: req.params.user_id }
+        });
+        res.status(200).json(dbArticlesData);
+    } catch (err) { res.status(422).json(err) }
 });
 
 module.exports = router;

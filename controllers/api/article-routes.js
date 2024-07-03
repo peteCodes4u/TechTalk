cc8928abf25e776b82d27fc1b983da2f2ac50f2f
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-07-03 18:26:43 -0400

    developed api for integration with UI

--- controllers/api/article-routes.js
+++ controllers/api/article-routes.js
@@ -27,5 +27,22 @@ router.put('/:id', async(req, res) => {
     } catch (err) {res.status(422).json(err)}
 });
 
+// create article
+router.post('/:user_id', async(req, res) => {
+    const user_id = 
+    // req.session.user_id;
+    req.params.user_id
+try{
+
+
+    await Article.create({
+        user_id: user_id,
+        title: req.body.title,
+        blog: req.body.blog,
+    });
+    return res.status(200).json({ message: "Yay! You made a new post! horay for you." });
+} catch(err){res.status(422).json(err)}
+});
+
 
 module.exports = router;

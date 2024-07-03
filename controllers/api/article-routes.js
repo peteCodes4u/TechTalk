a799c5330b8ce2225102783b878225add07ac725
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-07-03 14:08:39 -0400

    Competed API in article routes to accomodate for the update articles feature

--- controllers/api/article-routes.js
+++ controllers/api/article-routes.js
@@ -11,5 +11,21 @@ router.get('/', async (req, res) => {
     } catch (err) { res.status(500).json(err) }
 });
 
+// update articles
+router.put('/:id', async(req, res) => {
+    try {
+
+       const postData = {
+        title: req.body.title,
+        blog: req.body.blog,
+       } 
+
+        await Article.update(postData, { where: { id: req.params.id}});
+        
+        res.status(200).json({ message: 'you have successfully updated your post'})
+
+    } catch (err) {res.status(422).json(err)}
+});
+
 
 module.exports = router;
